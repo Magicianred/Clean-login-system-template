fb4646d8581307305ce59412f2ab03f391512952
Rajan G <rajangrg1@hotmail.com>
2020-05-01 10:49:50 +1200

    seperate Data Access Layer created

---
+++ Clean_Login_System_template/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Clean_Login_System_template.MvcApplication" Language="C#" %>
---
+++ Clean_Login_System_template/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clean_Login_System_template.Startup))]
+namespace Clean_Login_System_template
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
